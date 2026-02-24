3bf087411feb35c02179048033b14740ebd296e7
Adron Hall <adronhall@gmail.com>
2026-02-24 14:05:45 -0800

    Testing

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -51,6 +51,7 @@ export default async function AdminPage() {
       <ListBreadcrumbs items={breadcrumbItems} />
       <div className="row mb-4">
         <div className="col-12 d-flex justify-content-end gap-2">
+          <a href="/admin/email-logging" className="btn btn-outline-primary">
           <a href="/admin/support-links" className="btn btn-outline-primary">
             <i className="bx bx-link me-2"></i>Support Links
           </a>
